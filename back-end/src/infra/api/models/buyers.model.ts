615c3423577c5c25f29e75a944cf68fc31e686ab
kelderpassos <kelder.passos@gmail.com>
2022-12-27 11:20:04 -0300

    Refactor: added allowNull key and changed some types to the correct values

--- back-end/src/infra/api/models/buyers.model.ts
+++ back-end/src/infra/api/models/buyers.model.ts
@@ -38,80 +38,104 @@ Buyers.init({
   },
   name: {
     type: DataTypes.STRING,
-    allowNull: false,
+    allowNull: true,
   },
   tradingName: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   cashforceTax: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   responsibleName: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   responsibleEmail: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   responsiblePosition: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   responsiblePhone: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   responsibleMobile: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   website: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   postalCode: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   address: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   number: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   complement: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   neighborhood: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   city: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   state: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   phoneNumber: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   situation: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   situationDate: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
   createdAt: {
-    type: DataTypes.STRING,
+    type: DataTypes.DATE,
+    allowNull: false,
   },
   updatedAt: {
-    type: DataTypes.STRING,
+    type: DataTypes.DATE,
+    allowNull: false,
   },
   cnpjId: {
-    type: DataTypes.STRING,
+    type: DataTypes.INTEGER,
+    allowNull: true,
   },
   confirm: {
-    type: DataTypes.STRING,
+    type: DataTypes.TINYINT,
+    allowNull: false,
   },
   email: {
     type: DataTypes.STRING,
+    allowNull: true,
   },
 }, {
   sequelize: connection,
   timestamps: true,
   underscored: false,
   tableName: 'buyers',
+  charset: 'latin1',
 });

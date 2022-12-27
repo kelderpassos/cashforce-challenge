4b6dd1fc9cb05984f0b566a14dfb46d4efd339e7
kelderpassos <kelder.passos@gmail.com>
2022-12-27 11:13:16 -0300

    Feat: Sequelize model for table cnpjs created

---
+++ back-end/src/infra/api/models/cnpjs.model.ts
@@ -0,0 +1,40 @@
+import { DataTypes, Model } from 'sequelize';
+import connection from '../../database/connection';
+
+class CNPJS extends Model {
+  id!: number;
+  cnpj!: string;
+  companyType!: string;
+  createdAt!: Date;
+  updatedAt!: Date;
+}
+
+CNPJS.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  cnpj: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  companyType: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  updatedAt: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: connection,
+  timestamps: true,
+  underscored: false,
+  tableName: 'cnpjs',
+});

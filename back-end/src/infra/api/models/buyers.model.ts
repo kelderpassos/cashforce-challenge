2df7106345a76623a17ec8b72241754269f039c9
kelderpassos <kelder.passos@gmail.com>
2022-12-27 11:08:40 -0300

    Style: lint issues fixed

--- back-end/src/infra/api/models/buyers.model.ts
+++ back-end/src/infra/api/models/buyers.model.ts
@@ -1,5 +1,5 @@
 import { DataTypes, Model } from 'sequelize';
-import connection from '../../database/connection'
+import connection from '../../database/connection';
 
 class Buyers extends Model {
   id!: number;

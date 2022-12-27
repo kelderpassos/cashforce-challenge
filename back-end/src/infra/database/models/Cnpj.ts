923dc77e290aabe18277cbf3f280ae20da39c32b
kelderpassos <kelder.passos@gmail.com>
2022-12-27 16:46:03 -0300

    Feat: added relation hasMany with every related table. Lint rule needed to be deactivated.

--- back-end/src/infra/database/models/Cnpj.ts
+++ back-end/src/infra/database/models/Cnpj.ts
@@ -1,5 +1,10 @@
+/* eslint-disable import/no-cycle */
 import { DataTypes, Model } from 'sequelize';
 import connection from '../connection';
+import Buyers from './Buyers';
+import Orders from './Orders';
+import Providers from './Providers';
+import Sponsors from './Sponsors';
 
 export default class Cnpj extends Model {
   id!: number;
@@ -39,3 +44,8 @@ Cnpj.init({
   tableName: 'cnpjs',
   charset: 'latin1',
 });
+
+Cnpj.hasMany(Buyers, { foreignKey: 'id', as: 'buyers' });
+Cnpj.hasMany(Orders, { foreignKey: 'id', as: 'orders' });
+Cnpj.hasMany(Providers, { foreignKey: 'id', as: 'providers' });
+Cnpj.hasMany(Sponsors, { foreignKey: 'id', as: 'sponsors' });

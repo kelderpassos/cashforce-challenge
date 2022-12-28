c6c1a39af3c328c42d3cf57921728fbe337380d0
kelderpassos <kelder.passos@gmail.com>
2022-12-28 14:17:34 -0300

    Feat: Controller created to administrate requests and responses to the api. It sends all invoices related to a specific user

---
+++ back-end/src/infra/api/controllers/invoice.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import InvoiceModel from '../models/invoice.model';
+import RequestError from '../utils/RequestError';
+
+export default class InvoiceController {
+  constructor(protected _model = new InvoiceModel()) { }
+
+  findAll = async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
+
+    if (!id) throw new RequestError(404, 'Invalid id');
+
+    const allInvoices = await this._model.findAll(id);
+
+    if (!allInvoices) throw new RequestError(404, 'Any invoices found');
+
+    return res.status(200).json(allInvoices);
+  };
+}

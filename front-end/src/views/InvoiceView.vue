4a2ca8c5889fd219655dc0383f3d887aa1f1eb00
kelderpassos <kelder.passos@gmail.com>
2022-12-29 15:46:06 -0300

    Feat: unnecessary files removed. The most basic structure of InvoiceView was elaborated.

---
+++ front-end/src/views/InvoiceView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <aside>
+      <img />
+      <button>Notas Fiscais</button>
+    </aside>
+    <main>
+      <div>
+        <section>
+          <img src="" alt="" />
+          <h1>Notas Fiscais</h1>
+        </section>
+        <p>Visualize as notas fiscais que você tem</p>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>

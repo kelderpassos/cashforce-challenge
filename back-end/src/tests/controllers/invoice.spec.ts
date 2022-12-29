c500895eff5ca870d7da91975bd44f33893644df
kelderpassos <kelder.passos@gmail.com>
2022-12-29 12:01:33 -0300

    Test: first test made. It checks if the api returns the correct status code

--- back-end/src/tests/controllers/invoice.spec.ts
+++ back-end/src/tests/controllers/invoice.spec.ts
@@ -1,11 +1,15 @@
-import chai from 'chai';
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import chai, { expect } from 'chai';
 import * as sinon from 'sinon';
 import { Request, Response } from 'express';
 import InvoiceModel from '../../infra/api/models/invoice.model';
 import Orders from '../../infra/database/models/Orders';
 import { mockInvoice } from '../mocks';
+import InvoiceController from '../../infra/api/controllers/invoice.controller';
 
 describe('Endpoint /invoices', () => {
+  const invoiceModel = new InvoiceModel();
+
   const req = {} as Request;
   const res = {} as Response;
 
@@ -19,10 +23,17 @@ describe('Endpoint /invoices', () => {
   });
 
   describe('Get method', () => {
-    const invoiceModel = new InvoiceModel();
-
     it('should return status code "200" in case of sucess', async () => {
+      const invoiceController = new InvoiceController();
+      sinon.stub(invoiceModel, 'findAll').resolves(mockInvoice as unknown as Orders[]);
 
+      req.params = { id: '1' };
+
+      await invoiceController.findAll(req, res);
+
+      const resStub = res.status as sinon.SinonStub;
+
+      expect(resStub.calledWith(200)).to.be.true;
     });
   });
 });

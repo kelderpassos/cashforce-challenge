2dff7e858c6b2d9adc706d067510207fadccfe5e
kelderpassos <kelder.passos@gmail.com>
2022-12-30 18:43:42 -0300

    Feat: more styling added

--- front-end/src/components/InvoiceTable.vue
+++ front-end/src/components/InvoiceTable.vue
@@ -12,7 +12,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(invoice, i) in invoices" v-bind:key="i">
+        <tr v-for="(invoice, i) in invoices" v-bind:key="i" class="list">
           <td>{{ invoice.orderNumber }}</td>
           <td>{{ invoice.buyers.name }}</td>
           <td>{{ invoice.providers.name }}</td>
@@ -51,7 +51,7 @@ export default defineComponent({
   },
 });
 </script>
-<style>
+<style scoped>
 table {
   border-collapse: separate;
   border-spacing: 0 1rem;
@@ -63,11 +63,25 @@ th {
   font-size: 0.75rem;
   font-weight: 700;
   padding-bottom: 1rem;
+  width: 120px;
 }
+
+tr {
+  border: 1px solid #dfe2eb;
+  border-radius: 6px;
+  padding: 1rem;
+}
+
 td {
   color: #4d5566;
   font-size: 0.875rem;
   font-weight: 500;
   padding: 1rem 0;
+  margin-bottom: 1rem;
+}
+
+.list {
+  box-shadow: 1px 1px 1px 1px #dfe2eb;
+  border-radius: 0.5rem;
 }
 </style>

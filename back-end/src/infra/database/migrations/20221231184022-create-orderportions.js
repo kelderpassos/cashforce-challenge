6465a3bb729558fcd83142b4a1568a7fc7cadda0
kelderpassos <kelder.passos@gmail.com>
2022-12-31 15:46:11 -0300

    Feat: migrations created through cli

---
+++ back-end/src/infra/database/migrations/20221231184022-create-orderportions.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('orderportions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER(11)
+      },
+      nDup: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      dVenc: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      vDup: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      availableToMarket: {
+        defaultValue: 1,
+        type: Sequelize.TINYINT(1)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      orderId: {
+        defaultValue: null,
+        type: Sequelize.INTEGER(11),
+        references: {
+          model: 'orders',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        key: 'orderId'
+      }
+    }, {
+      charset: 'latin1'
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('orderportions');
+  }
+};
